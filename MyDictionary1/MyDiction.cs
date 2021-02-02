2b148e0fd267b274577b4d1aa61ce0a1068c0b78
umran <ind.eng.umran@gmail.com>
2021-02-02 18:33:23 +0300

    Add project files.

---
+++ MyDictionary1/MyDiction.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MyDictionary1
+{
+    public class MyDiction<TK, TV>
+    {
+        TK[] keys;
+        TV[] values;
+        public MyDiction()
+        {
+            keys = new TK[0];
+            values = new TV[0];
+            ;
+        }
+
+        public void Add(TK key, TV value)
+        {
+            TK[] tempKey = keys;
+            TV[] tempValue = values;
+
+            keys = new TK[keys.Length + 1];
+            values = new TV[values.Length + 1];
+            for (int i = 0; i < tempKey.Length; i++)
+            {
+                keys[i] = tempKey[i];
+
+            }
+            keys[keys.Length - 1] = key;
+
+            for (int i = 0; i < tempValue.Length; i++)
+            {
+                values[i] = tempValue[i];
+
+            }
+            values[values.Length - 1] = value;
+            Console.WriteLine("product id:" + key + "----" + "product name:" + value);
+        }
+
+    }
+}
---
+++ MyDictionary1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MyDictionary1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDiction<int, string> products = new MyDiction<int, string>();
+            products.Add(1, "pencil");
+            products.Add(2, "ruler");
+        }
+    }
+}
